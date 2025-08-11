1f8b9a4655b88bc2461c03809084f0fe7b9d8125
MitGoswami <mitg59@gmail.com>
2025-08-12 00:08:31 +0530

    removing host, port

--- MYSQL_server/main.py
+++ MYSQL_server/main.py
@@ -15,12 +15,13 @@ logger = logging.getLogger(__name__)
 # Entry point to run the server
 if __name__ == "__main__":
     # Azure assigns a random port to your container; fetch from env
-    port = int(os.getenv("PORT",8080))
-    host = "0.0.0.0"  # listen on all interfaces
-    logger.info(f"Starting MCP server on {host}:{port}")
-    logger.info(f"Environment PORT: {os.getenv('PORT')}")
+    # port = int(os.getenv("PORT",8080))
+    # host = "0.0.0.0"  # listen on all interfaces
+    # logger.info(f"Starting MCP server on {host}:{port}")
+    # logger.info(f"Environment PORT: {os.getenv('PORT')}")
     try:
-        mcp.run(host=host, port=port)
+        mcp.run()
+        logger.info(f"MCP server started")
     except Exception as e:
         logger.error(f"Failed to start server: {e}")
         raise
