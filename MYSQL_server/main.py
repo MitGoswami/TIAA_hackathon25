40849226a8c123aa8c770c9de1f1304c31efac3e
MitGoswami <mitg59@gmail.com>
2025-08-11 15:34:36 +0530

    adding default port

--- MYSQL_server/main.py
+++ MYSQL_server/main.py
@@ -7,6 +7,6 @@ import prompts.schema_prompt
 # Entry point to run the server
 if __name__ == "__main__":
     # Azure assigns a random port to your container; fetch from env
-    port = int(os.getenv("PORT",8000))
+    port = int(os.getenv("PORT",8080))
     host = "0.0.0.0"  # listen on all interfaces
     mcp.run_http(host=host, port=port)
